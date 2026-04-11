e0dccf700e4ab5e183c8fad6e229c09b3ea0a771
小丽花 🌸 <73592650+nayraoviedo@users.noreply.github.com>
2026-04-11 09:14:45 -0400

    Firebase-prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/app_main_screen.dart';
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -22,23 +26,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Recetas 🍲"),
-      ),
+      appBar: AppBar(title: const Text("Recetas 🍲")),
       body: ListView(
         children: const [
-          ListTile(
-            title: Text("🍕 Pizza"),
-            subtitle: Text("Deliciosa pizza"),
-          ),
-          ListTile(
-            title: Text("🍝 Pasta"),
-            subtitle: Text("Pasta italiana"),
-          ),
-          ListTile(
-            title: Text("🍔 Hamburguesa"),
-            subtitle: Text("Con queso"),
-          ),
+          ListTile(title: Text("🍕 Pizza"), subtitle: Text("Deliciosa pizza")),
+          ListTile(title: Text("🍝 Pasta"), subtitle: Text("Pasta italiana")),
+          ListTile(title: Text("🍔 Hamburguesa"), subtitle: Text("Con queso")),
         ],
       ),
     );
