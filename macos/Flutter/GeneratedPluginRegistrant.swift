d072ca4d3ee1854be0d26f5edf83e74595218d2b
Carlos La Fuente <carlos.aurelio.la.ca@gmail.com>
2026-04-13 12:27:25 -0400

    Ajustes post-merge y correcciones

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,5 @@ import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-import firebase_core
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
