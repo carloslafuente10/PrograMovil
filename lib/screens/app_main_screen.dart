d4daee573ef9ffa2dd09763fe12ac6dcc42bfb6b
lynn7198-maker <lynn71798@gmail.com>
2026-04-13 21:55:25 -0400

    conflictos resueltos integracion login + frontend

--- lib/screens/app_main_screen.dart
+++ lib/screens/app_main_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
- Updated upstream
+
 import '../servicios/api_servicio.dart';
 
 import 'home_screen.dart';
- Stashed changes
+
+
 
 class AppMainScreen extends StatefulWidget {
   const AppMainScreen({super.key});
@@ -20,7 +21,7 @@ class _AppMainScreenState extends State<AppMainScreen> {
   @override
   void initState() {
     page = [
- Updated upstream
+
       // 🔥 HOME (recetas desde backend)
       FutureBuilder(
         future: ApiService.getRecetas(),
@@ -83,7 +84,7 @@ class _AppMainScreenState extends State<AppMainScreen> {
       // 🔥 otras pantallas
 
       HomeScreen(),
- Stashed changes
+
       const Center(child: Text("Favoritos")),
       const Center(child: Text("Plan")),
       const Center(child: Text("Configuración")),
