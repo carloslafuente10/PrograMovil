10716fce3d35dc8704d764ba32aa118b2f5d6d1d
Carlos La Fuente <carlos.aurelio.la.ca@gmail.com>
2026-04-13 21:08:48 -0400

    Resolviendo conflicto de front-lenny

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-<<<<<<< Updated upstream
 
 import 'firebase_options.dart';
 import 'screens/login_page.dart';
-=======
-import 'firebase_options.dart';
->>>>>>> Stashed changes
 import 'screens/app_main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-<<<<<<< Updated upstream
     options: DefaultFirebaseOptions.currentPlatform,
-=======
-    options:
-    DefaultFirebaseOptions.currentPlatform,
->>>>>>> Stashed changes
   );
 
   runApp(const MyApp());
@@ -29,20 +20,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp( // ❗ quitamos const
       debugShowCheckedModeBanner: false,
-<<<<<<< Updated upstream
       title: 'App Recetas',
       theme: ThemeData(
         primarySwatch: Colors.orange,
         useMaterial3: true,
       ),
-      // Puedes cambiar aquí entre Login o App principal
-      home: const LoginPage(),
+
+      // 👉 ELIGE SOLO UNA
+      //home: const LoginPage(),
       // home: const AppMainScreen(),
-=======
-      home: AppMainScreen(),
->>>>>>> Stashed changes
+       home: const AppMainScreen(),
     );
   }
 }
