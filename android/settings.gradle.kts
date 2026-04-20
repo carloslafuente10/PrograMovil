80221a9484b601d8adc568a3c226fff4805cb409
Valmier1079 <barley1079@gmail.com>
2026-04-20 17:14:20 -0300

    Cambio de color a verde esmeralda en Login y Registro para consistencia visual

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,16 +19,9 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    // START: FlutterFire Configuration
-<<<<<<< Updated upstream
-    id("com.google.gms.google-services") version("4.4.4") apply false
-    // END: FlutterFire Configuration 4.3.15
-=======
-    id("com.google.gms.google-services") version("4.3.15") apply false
-    // END: FlutterFire Configuration
->>>>>>> Stashed changes
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.android.application") version "8.3.0" apply false // DEBE SER 8.2.1 O SUPERIOR
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false 
 }
 
 include(":app")
